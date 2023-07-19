e23e0eba045346e7a962e3194adbcde2a397e091
musashifraco <ezequielestudos11@gmail.com>
2023-07-19 13:13:13 -0300

    Criada a rota Lists.

--- src/utils/configs/Router.tsx
+++ src/utils/configs/Router.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { ListaDeGames } from "../../pages/ListaDeGames";
 import { Game } from "../../pages/Game";
+import { Lists } from "../../pages/Lists";
 export function Router() {
     return (
         <Routes>
+            <Route path="/lists" element={<Lists/>}></Route>
             <Route path="/lists/:listId/games" element={<ListaDeGames/>}></Route>
             <Route path="/lists/:listId/games/:gameId" element={<Game/>}></Route>
         </Routes>
