737e5760457db765c7c94556f3cbf89d440e356b
musashifraco <ezequielestudos11@gmail.com>
2023-07-25 08:33:38 -0300

    criando navegacao entre as rotas

--- src/components/pages/home/Intro.styles.ts
+++ src/components/pages/home/Intro.styles.ts
@@ -1,70 +1,79 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const IntroMain = styled.main`
-display: flex;
-justify-content: left;
-align-items: center;
+  display: flex;
+  justify-content: left;
+  align-items: center;
 
-height: 100%;
-width: 100%;
+  height: 100%;
+  width: 100%;
 
-background-color: var(--blue-900);
+  background-color: var(--blue-900);
 
-    img {
-        position: absolute;
-        right: 300px;
+  img {
+    position: absolute;
+    right: 300px;
 
-        height: 550px;
-    }
-`
+    height: 550px;
+  }
+`;
 
 export const TextContainer = styled.div`
-z-index: 100;
+  z-index: 100;
 
-display: flex;
-justify-content: center;
-flex-direction: column;
-transform: translateX(60%);
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  transform: translateX(60%);
 
-height: 550px;
-width: 450px;
+  height: 550px;
+  width: 450px;
 
+  word-break: break-all;
 
-word-break: break-all;
-
-    span {
+  span {
     font-weight: 700;
     font-size: 30px;
-    color: var(--blue-400)
-    }
+    color: var(--blue-400);
+  }
 
-    h1, p {
+  h1,
+  p {
     color: var(--gray-100);
-    }
+  }
 
-    h1 {
+  h1 {
     padding-right: 50px;
     margin-bottom: 25px;
     font-size: 60px;
-    }
+  }
 
-    p {
+  p {
     margin-bottom: 40px;
 
     font-weight: 500;
     font-size: 20px;
+  }
 
-    }
-
-button {
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     height: 60px;
-    border: none;
     border-radius: 10px;
 
     background-color: var(--blue-400);
+  }
+
+  button {
+    height: 100%;
+    width: 100%;
+    border: none;
+    
+    background-color: transparent;
 
     color: var(--blue-900);
     font-weight: 500;
     font-size: 25px;
-    }
-`
+  }
+`;
